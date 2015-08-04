636ca3b25f4ea32888cc9b81f6252facf5e88e86
Christopher Condit <condit@sdsc.edu>
2015-08-04 11:32:48 -0700

    Switches to test rules #1

--- src/main/java/io/scigraph/ontomatcher/OntologyMatcher.java
+++ src/main/java/io/scigraph/ontomatcher/OntologyMatcher.java
@@ -15,66 +15,33 @@
  */
 package io.scigraph.ontomatcher;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
 import javax.inject.Inject;
 
-import org.apache.commons.lang.StringUtils;
-import org.apache.commons.math3.distribution.HypergeometricDistribution;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
-import org.neo4j.graphdb.Result;
-import org.neo4j.graphdb.Transaction;
 import org.neo4j.tooling.GlobalGraphOperations;
 
-import com.google.common.base.Optional;
-
 import edu.sdsc.scigraph.annotation.EntityProcessor;
-import edu.sdsc.scigraph.frames.CommonProperties;
-import edu.sdsc.scigraph.frames.NodeProperties;
-import edu.sdsc.scigraph.internal.CypherUtil;
-import edu.sdsc.scigraph.neo4j.Graph;
-import edu.sdsc.scigraph.owlapi.curies.CurieUtil;
 
 public class OntologyMatcher {
 
   private final GraphDatabaseService graphDb;
-  private final CurieUtil curieUtil;
-  private final Graph graph;
-  private final CypherUtil cypherUtil;
   private final GlobalGraphOperations graphOps;
-  
-  @Inject
-  private EntityProcessor processor;
-
+  private final EntityProcessor processor;
 
   @Inject
-  OntologyMatcher(GraphDatabaseService graphDb, CurieUtil curieUtil, Graph graph,
-      CypherUtil cypherUtil, EntityProcessor processor) {
+  OntologyMatcher(GraphDatabaseService graphDb, EntityProcessor processor) {
     this.graphDb = graphDb;
     graphOps =  GlobalGraphOperations.at(graphDb);
-    this.curieUtil = curieUtil;
-    this.graph = graph;
-    this.cypherUtil = cypherUtil;
     this.processor = processor;
   }
 
   public void matchAll() {
-	  System.out.println("Matching all");
-	  System.out.println("Processor: "+processor);
-	  graphDb.beginTx();
-	  for (Node n : graphOps.getAllNodes()) {
-		  System.out.println("NODE="+n);
-		  
-	  }
-	  
-	  
+    System.out.println("Matching all");
+    System.out.println("Processor: "+processor);
+    graphDb.beginTx();
+    for (Node n : graphOps.getAllNodes()) {
+      System.out.println("NODE="+n);
+    }
   }
 }
--- src/test/java/io/scigraph/ontomatcher/OntologyMatcherTest.java
+++ src/test/java/io/scigraph/ontomatcher/OntologyMatcherTest.java
@@ -15,90 +15,26 @@
  */
 package io.scigraph.ontomatcher;
 
-import static com.google.common.collect.Sets.newHashSet;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.empty;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.not;
+import static org.mockito.Mockito.mock;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.junit.BeforeClass;
+import org.junit.Before;
+import org.junit.ClassRule;
 import org.junit.Test;
-import org.neo4j.graphdb.Transaction;
-import org.semanticweb.owlapi.apibinding.OWLManager;
-import org.semanticweb.owlapi.model.IRI;
-import org.semanticweb.owlapi.model.OWLOntologyManager;
-import org.semanticweb.owlapi.util.OWLOntologyWalker;
-
-import com.google.common.io.Resources;
-import com.google.inject.Guice;
-import com.google.inject.Injector;
 
 import edu.sdsc.scigraph.annotation.EntityProcessor;
-import edu.sdsc.scigraph.frames.NodeProperties;
-import edu.sdsc.scigraph.internal.CypherUtil;
-import edu.sdsc.scigraph.neo4j.Neo4jModule;
-import edu.sdsc.scigraph.owlapi.GraphOwlVisitor;
-import edu.sdsc.scigraph.owlapi.OwlPostprocessor;
-import edu.sdsc.scigraph.owlapi.curies.CurieUtil;
-import edu.sdsc.scigraph.owlapi.loader.OwlLoadConfiguration;
-import edu.sdsc.scigraph.owlapi.loader.OwlLoaderModule;
-import edu.sdsc.scigraph.owlapi.loader.OwlLoadConfiguration.MappedProperty;
-import edu.sdsc.scigraph.util.GraphTestBase;
+import edu.sdsc.scigraph.owlapi.OntologyGraphRule;
 
-public class OntologyMatcherTest extends GraphTestBase {
+public class OntologyMatcherTest {
 
-  static OntologyMatcher matcher;
-  static CurieUtil util;
+  @ClassRule
+  public static OntologyGraphRule ontologyGraph = new OntologyGraphRule("pizza.owl");
 
-  @BeforeClass
-  public static void loadPizza() throws Exception {
-	  OwlLoadConfiguration config;
-	  Injector i =
-			  Guice.createInjector(new OwlLoaderModule(config), new Neo4jModule(config.getGraphConfiguration()));
+  OntologyMatcher matcher;
 
-    //Injector injector = Guice.createInjector(new OntologyMatcherModule());
-	EntityProcessor processor = injector.getInstance(EntityProcessor.class);
-
-    OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
-    String uri = Resources.getResource("pizza.owl").toURI().toString();
-    IRI iri = IRI.create(uri);
-    manager.loadOntologyFromOntologyDocument(iri);
-    OWLOntologyWalker walker = new OWLOntologyWalker(manager.getOntologies());
-
-    MappedProperty mappedProperty = new MappedProperty(NodeProperties.LABEL);
-    List<String> properties = new ArrayList<String>();
-    properties.add("http://www.w3.org/2000/01/rdf-schema#label");
-    properties.add("http://www.w3.org/2004/02/skos/core#prefLabel");
-    mappedProperty.setProperties(properties);
-
-    ArrayList<MappedProperty> mappedPropertyList = new ArrayList<MappedProperty>();
-    mappedPropertyList.add(mappedProperty);
-
-    GraphOwlVisitor visitor = new GraphOwlVisitor(walker, graph, mappedPropertyList);
-    walker.walkStructure(visitor);
-    Map<String, String> categories = new HashMap<>();
-    categories.put("http://www.co-ode.org/ontologies/pizza/pizza.owl#NamedPizza", "pizza");
-    categories.put("http://www.co-ode.org/ontologies/pizza/pizza.owl#PizzaTopping", "topping");
-    try (Transaction tx = graphDb.beginTx()) {
-      OwlPostprocessor postprocessor = new OwlPostprocessor(graphDb, categories);
-      postprocessor.processCategories(categories);
-      postprocessor.processSomeValuesFrom();
-      tx.success();
-    }
-
-    Map<String, String> map = new HashMap<>();
-    map.put("pizza", "http://www.co-ode.org/ontologies/pizza/pizza.owl#");
-    util = new CurieUtil(map);
-    CypherUtil cypherUtil = new CypherUtil(graphDb, util);
-    
-
- 
-    matcher = new OntologyMatcher(graphDb, util, graph, cypherUtil, processor);
+  @Before
+  public void setup() throws Exception {
+    EntityProcessor processor = mock(EntityProcessor.class);
+    matcher = new OntologyMatcher(ontologyGraph.getGraphDb(), processor);
   }
 
   @Test
